7ee878df6aaf287906bead42fa56d9d8ab87ece8
felix1801 <noiset.lapin@hotmail.fr>
2024-08-30 14:17:37 +0200

    Initial commit

---
+++ pipelines/utils/helpers.py
@@ -0,0 +1,34 @@
+from kedro.io import DataCatalog
+from kedro.config import OmegaConfigLoader
+from pathlib import Path
+
+import time
+from contextlib import contextmanager
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+@contextmanager
+def timer(title):
+    t0 = time.time()
+    yield
+    print("{} - done in {:.0f}s".format(title, time.time() - t0))
+
+# One-hot encoding for categorical columns with get_dummies
+def one_hot_encoder(df, nan_as_category = True):
+    original_columns = list(df.columns)
+    categorical_columns = [col for col in df.columns if df[col].dtype == 'object']
+    df = pd.get_dummies(df, columns= categorical_columns, dummy_na= nan_as_category)
+    new_columns = [c for c in df.columns if c not in original_columns]
+    return df, new_columns
+
+# Display/plot feature importance
+def display_importances(feature_importance_df_):
+    cols = feature_importance_df_[["feature", "importance"]].groupby("feature").mean().sort_values(by="importance", ascending=False)[:40].index
+    best_features = feature_importance_df_.loc[feature_importance_df_.feature.isin(cols)]
+    plt.figure(figsize=(8, 10))
+    sns.barplot(x="importance", y="feature", data=best_features.sort_values(by="importance", ascending=False))
+    plt.title('LightGBM Features (avg over folds)')
+    plt.tight_layout()
+    plt.savefig('lgbm_importances01.png')
---
+++ run_mlflow_ui.py
@@ -0,0 +1,5 @@
+import mlflow
+import os
+
+if __name__ == "__main__":
+    os.system("mlflow ui")
